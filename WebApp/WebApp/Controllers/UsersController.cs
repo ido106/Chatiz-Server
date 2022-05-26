0529c9c94e757e77e6c58320e8c1d07647d12843
Ido Aharon <idoah106@gmail.com>
2022-05-27 00:20:23 +0300

    Authorize

--- WebApp/WebApp/Controllers/UsersController.cs
+++ WebApp/WebApp/Controllers/UsersController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Domain;
 using Repository;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebApp.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly WebAppContext _context;
