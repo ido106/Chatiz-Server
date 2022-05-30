e2c62e4e17fb84519bba868cfc7630a6897a199a
Ido Aharon <idoah106@gmail.com>
2022-05-30 13:43:30 +0300

    code dissapear fix

--- WebApp/Repository/Migrations/20220530103319_NewContact.Designer.cs
+++ WebApp/Repository/Migrations/20220530103319_NewContact.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -10,9 +11,10 @@ using Repository;
 namespace Repository.Migrations
 {
     [DbContext(typeof(WebAppContext))]
-    partial class WebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20220530103319_NewContact")]
+    partial class NewContact
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
