41c59b72e6be52c16ae5549f6b64bd8b559425e0
Ido Aharon <idoah106@gmail.com>
2022-05-30 13:30:42 +0300

    change user model

--- WebApp/WebApi/Program.cs
+++ WebApp/WebApi/Program.cs
@@ -13,13 +13,8 @@ builder.Services.AddControllers();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddSignalR();
-
-
-
 
 /**
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
@@ -101,11 +96,11 @@ if (app.Environment.IsDevelopment())
 
 
 
-/*app.UseCors(x => x
+app.UseCors(x => x
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .SetIsOriginAllowed(origin => true) // allow any origin
-                    .AllowCredentials()); // allow credentials*/
+                    .AllowCredentials()); // allow credentials
 
 app.UseCors("Allow All");
 app.UseHttpsRedirection();
@@ -113,6 +108,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-// app.MapHub<ChatHub>("/ChatHub");
 
 app.Run();
